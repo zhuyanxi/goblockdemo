4a028d5620588790f67fc05b6b6c61cff18b52f8
yanxi zhu <pacis2014@gmail.com>
2018-05-08 00:01:38 +0800

    20180508-0:01 at home

---
+++ installcmd.sh
@@ -0,0 +1,13 @@
+https_proxy=http://127.0.0.1:27315 go get -u -v github.com/nsf/gocode
+https_proxy=http://127.0.0.1:27315 go get -u -v github.com/ramya-rao-a/go-outline
+https_proxy=http://127.0.0.1:27315 go get -u -v github.com/newhook/go-symbols
+https_proxy=http://127.0.0.1:27315 go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
+https_proxy=http://127.0.0.1:27315 go get -u -v golang.org/x/tools/cmd/guru
+https_proxy=http://127.0.0.1:27315 go get -u -v golang.org/x/tools/cmd/gorename
+https_proxy=http://127.0.0.1:27315 go get -u -v sourcegraph.com/sqs/goreturns
+https_proxy=http://127.0.0.1:27315 go get -u -v github.com/rogpeppe/godef
+https_proxy=http://127.0.0.1:27315 go get -u -v golang.org/x/tools/cmd/godoc
+https_proxy=http://127.0.0.1:27315 go get -u -v github.com/golang/lint/golint
+https_proxy=http://127.0.0.1:27315 go get -u -v github.com/derekparker/delve/cmd/dlv
+https_proxy=http://127.0.0.1:27315 go get -u -v github.com/fatih/gomodifytags
+https_proxy=http://127.0.0.1:27315 go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
