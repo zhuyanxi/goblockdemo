97772ad8d8efc8e8ecaae61393c8dc314b74177f
yanxi zhu <547110356@qq.com>
2018-05-21 16:53:28 +0800

    no comment

--- block/blockchain_db.go
+++ block/blockchain_db.go
@@ -1,58 +1,58 @@
 package block
 
-// import (
-// 	"encoding/hex"
-// 	"log"
+import (
+	"encoding/hex"
+	"log"
 
-// 	"github.com/zemirco/couchdb"
-// 	"github.com/zhuyanxi/goblockdemo/util"
-// )
+	"github.com/zemirco/couchdb"
+	"github.com/zhuyanxi/goblockdemo/util"
+)
 
-// const dbName = "blockchain"
+const dbName = "blockchain"
 
-// // BlockChain :
-// type BlockChain struct {
-// 	tipHash []byte
-// }
+// BlockChain :
+type BlockChain struct {
+	tipHash []byte
+}
 
-// type BlockChainDB struct {
-// 	couchdb.Document
-// 	BCMap map[string]BlockChain
-// }
+type BlockChainDB struct {
+	couchdb.Document
+	BCMap map[string]BlockChain
+}
 
-// // newGenesisBlock :
-// func newGenesisBlock() *Block {
-// 	return NewBlock(0, "The First Block", []byte{}) //6929, 22949
-// }
+// newGenesisBlock :
+func newGenesisBlock() *Block {
+	return NewBlock(0, "The First Block", []byte{}) //6929, 22949
+}
 
-// // NewBlockChain :
-// func NewBlockChain() *BlockChain {
-// 	var tipHash []byte
-// 	client := util.DBClient()
-// 	db, err := client.Get(dbName)
-// 	if db == nil {
-// 		log.Println(err)
-// 		client.Create(dbName)
-// 		_db := client.Use(dbName)
+// NewBlockChain :
+func NewBlockChain() *BlockChain {
+	var tipHash []byte
+	client := util.DBClient()
+	db, err := client.Get(dbName)
+	if db == nil {
+		log.Println(err)
+		client.Create(dbName)
+		_db := client.Use(dbName)
 
-// 		gene := newGenesisBlock()
-// 		bcDB := &BlockChainDB{BCMap: map[string]BlockChain{
-// 			"tip_hash": BlockChain{tipHash: gene.Hash},
-// 		}}
-// 		_db.Post(bcDB)
+		gene := newGenesisBlock()
+		bcDB := &BlockChainDB{BCMap: map[string]BlockChain{
+			"tip_hash": BlockChain{tipHash: gene.Hash},
+		}}
+		_db.Post(bcDB)
 
-// 		blkDB := &BlockDB{
-// 			BlockMap: map[string]Block{
-// 				hex.EncodeToString(gene.Hash): *gene,
-// 			},
-// 		}
-// 		_db.Post(blkDB)
+		blkDB := &BlockDB{
+			BlockMap: map[string]Block{
+				hex.EncodeToString(gene.Hash): *gene,
+			},
+		}
+		_db.Post(blkDB)
 
-// 		tipHash = gene.Hash
-// 	} else {
+		tipHash = gene.Hash
+	} else {
 
-// 	}
+	}
 
-// 	bc := BlockChain{tipHash: tipHash}
-// 	return &bc
-// }
+	bc := BlockChain{tipHash: tipHash}
+	return &bc
+}
