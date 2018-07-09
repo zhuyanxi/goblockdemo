2d021d70c46502415c58821d1dbc1da06ef1ab52
yanxi zhu <547110356@qq.com>
2018-07-09 23:54:13 +0800

    no comment

--- block/transaction.go
+++ block/transaction.go
@@ -1,5 +1,15 @@
 package block
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+	"log"
+)
+
+const subsidy = 10
+
 // Transaction :
 type Transaction struct {
 	ID   []byte
@@ -18,4 +28,31 @@ type TXInput struct {
 type TXOutput struct {
 	Value        int
 	ScriptPubKey string
+}
+
+// SetID : set the id of transaction
+func (tx *Transaction) SetID() {
+	var encoded bytes.Buffer
+	var hash [32]byte
+
+	enc := gob.NewEncoder(&encoded)
+	err := enc.Encode(tx)
+	if err != nil {
+		log.Panic(err)
+	}
+	hash = sha256.Sum256(encoded.Bytes())
+	tx.ID = hash[:]
+}
+
+// NewCoinBase :
+func NewCoinBase(to, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Reward to '%s'", to)
+	}
+
+	txin := TXInput{[]byte{}, -1, data}
+	txout := TXOutput{subsidy, to}
+	tx := Transaction{nil, []TXInput{txin}, []TXOutput{txout}}
+	tx.SetID()
+	return &tx
 }
