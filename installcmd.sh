43b468be5aa884e013675447e831503f19f5c5dc
Zhu Yan Xi <yan-xi.zhu@ubisoft.com>
2019-12-05 18:17:42 +0800

    Update installcmd.sh

--- installcmd.sh
+++ installcmd.sh
@@ -1,19 +1,21 @@
 ## The golang packages
 
-https_proxy=http://127.0.0.1:27315 go get -u -v github.com/nsf/gocode
-https_proxy=http://127.0.0.1:27315 go get -u -v github.com/ramya-rao-a/go-outline
-https_proxy=http://127.0.0.1:27315 go get -u -v github.com/newhook/go-symbols
-https_proxy=http://127.0.0.1:27315 go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
-https_proxy=http://127.0.0.1:27315 go get -u -v golang.org/x/tools/cmd/guru
-https_proxy=http://127.0.0.1:27315 go get -u -v golang.org/x/tools/cmd/gorename
-https_proxy=http://127.0.0.1:27315 go get -u -v sourcegraph.com/sqs/goreturns
-https_proxy=http://127.0.0.1:27315 go get -u -v github.com/rogpeppe/godef
-https_proxy=http://127.0.0.1:27315 go get -u -v golang.org/x/tools/cmd/godoc
-https_proxy=http://127.0.0.1:27315 go get -u -v github.com/golang/lint/golint
-https_proxy=http://127.0.0.1:27315 go get -u -v github.com/derekparker/delve/cmd/dlv
-https_proxy=http://127.0.0.1:27315 go get -u -v github.com/fatih/gomodifytags
-https_proxy=http://127.0.0.1:27315 go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v github.com/mdempsky/gocode
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v github.com/ramya-rao-a/go-outline
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v github.com/newhook/go-symbols
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v golang.org/x/tools/cmd/guru
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v golang.org/x/tools/cmd/gorename
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v sourcegraph.com/sqs/goreturns
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v github.com/rogpeppe/godef
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v golang.org/x/tools/cmd/godoc
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v github.com/golang/lint/golint
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v github.com/derekparker/delve/cmd/dlv
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v github.com/fatih/gomodifytags
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
 
-https_proxy=http://127.0.0.1:27315 go get -u -v github.com/zemirco/couchdb
-https_proxy=http://127.0.0.1:27315 go get -u -v gopkg.in/couchbase/gocb.v1
-https_proxy=http://127.0.0.1:27315 go get -u -v github.com/couchbase/gocb
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v github.com/zemirco/couchdb
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v gopkg.in/couchbase/gocb.v1
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v github.com/couchbase/gocb
+
+https_proxy=http://proxy-mtl.ubisoft.org:3128 go get -u -v github.com/acroca/go-symbols
